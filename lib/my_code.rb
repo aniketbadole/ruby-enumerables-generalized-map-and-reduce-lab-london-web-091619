833f60f8b348cec2582c0efad1672547db7f1179
Aniket B <aniket.b@outlook.com>
2019-09-05 05:10:16 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,13 @@
-# Your Code Here
+def map(array)
+  arr = []
+  i = 0
+  while i < array.length
+    arr.push(yield array[i])
+    i += 1
+  end
+  arr
+end
+
+def reduce(array, starting_point = nil)
+  
+end
